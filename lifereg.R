43927857d2196fe96c7b5ad86cb14ceff258e40e
rajiviyengar <rajiviyengar@me.com>
2022-11-23 12:57:23 -0800

    Update lifereg.R

--- lifereg.R
+++ lifereg.R
@@ -5,5 +5,11 @@ who <- who[who$Year == 2015,]
 who[who$Status == 'Developing', 'Status'] <- 0
 who[who$Status == 'Developed', 'Status'] <- 1
 
-#Drop alcohol col, too many NA
-who <- who[, c(1:6, 8:22)]
+#Drop alcohol, totalexp col, too many NA
+who <- who[, c(1:6, 8:13, 15:22)]
+
+#import leaps for model/variable selection
+library(leaps)
+forward <- regsubsets(x = who[, c(3, )])
+
+#plot all predictors against response
